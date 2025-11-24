11be258e6d58b5f675d8288e99278031ff77a9bf
mako <a20210646@pucp.edu.pe>
2025-11-24 18:40:19 -0500

    FIX cache

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: '',
+    renderMode: RenderMode.Client
+  },
+  {
     path: 'products/:id',
     renderMode: RenderMode.Server
   },
