fe2582aad5a766bf0234f9173d39ef599af31db8
mako <a20210646@pucp.edu.pe>
2025-11-24 16:13:51 -0500

    Firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,22 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { initializeApp } from "firebase/app";
+import { getAnalytics, isSupported } from "firebase/analytics";
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(), provideClientHydration(withEventReplay())
   ]
 };
+
+export const app = initializeApp(environment.firebase);
+
+// Initialize analytics only if supported (browser environment)
+let analytics;
+isSupported().then(yes => yes ? analytics = getAnalytics(app) : null);
+export { analytics };
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDHL3QsnpPwXYRt8dU5uHb9J07YDr3GgEo",
+    authDomain: "cielosweb.firebaseapp.com",
+    projectId: "cielosweb",
+    storageBucket: "cielosweb.firebasestorage.app",
+    messagingSenderId: "914832571804",
+    appId: "1:914832571804:web:af687de88ecb67f6319c59",
+    measurementId: "G-K5VDHHLXK7"
+  }
+};
